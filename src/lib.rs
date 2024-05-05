709c114596e0f19689a799b1e8fc524aa6beb04b
ThailanSwaity <th4lans@gmail.com>
2024-05-05 09:27:38 -0500

    completed

---
+++ src/lib.rs
@@ -0,0 +1,107 @@
+use crossterm::{cursor, style, terminal, QueueableCommand};
+use rand::Rng;
+use std::error::Error;
+use std::io::{self, Write};
+use std::{thread, time};
+
+pub struct GameOfLife {
+    pub rows: usize,
+    pub cols: usize,
+    pub grid: Vec<Vec<bool>>,
+}
+
+impl GameOfLife {
+    pub fn new(rows: usize, cols: usize) -> Self {
+        GameOfLife {
+            rows,
+            cols,
+            grid: vec![vec![false; rows]; cols],
+        }
+    }
+
+    pub fn tick(&mut self) {
+        let mut t_grid: Vec<Vec<bool>> = vec![vec![false; self.rows]; self.cols];
+
+        for x in 0..self.cols {
+            for y in 0..self.rows {
+                let neighbours = self.get_neighbours(x as u16, y as u16);
+
+                if self.grid[x][y] && neighbours < 2 {
+                    t_grid[x][y] = false;
+                } else if self.grid[x][y] && (neighbours == 2 || neighbours == 3) {
+                    t_grid[x][y] = true;
+                } else if self.grid[x][y] && neighbours > 3 {
+                    t_grid[x][y] = false;
+                } else if !self.grid[x][y] && neighbours == 3 {
+                    t_grid[x][y] = true;
+                }
+            }
+        }
+
+        self.grid = t_grid;
+    }
+
+    fn get_neighbours(&self, x: u16, y: u16) -> u16 {
+        let mut neighbours = 0;
+        for dx in -1..=1 {
+            for dy in -1..=1 {
+                if dx == 0 && dy == 0 {
+                    continue;
+                }
+
+                let ax: i16 = x as i16 + dx;
+                let ay: i16 = y as i16 + dy;
+
+                if ax < 0 || ax >= self.cols as i16 || ay < 0 || ay >= self.rows as i16 {
+                    continue;
+                }
+
+                if self.grid[ax as usize][ay as usize] {
+                    neighbours += 1;
+                }
+            }
+        }
+        neighbours
+    }
+
+    pub fn is_extinct(&self) -> bool {
+        for x in 0..self.cols {
+            for y in 0..self.rows {
+                if self.grid[x][y] {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+pub fn run(mut game_grid: GameOfLife) -> Result<(), Box<dyn Error>> {
+    for x in 0..game_grid.cols {
+        for y in 0..game_grid.rows {
+            game_grid.grid[x][y] = rand::thread_rng().gen_bool(0.2);
+        }
+    }
+
+    let mut stdout = io::stdout();
+    // Run until the last cell dies
+    while !game_grid.is_extinct() {
+        game_grid.tick();
+
+        stdout.queue(terminal::Clear(terminal::ClearType::All))?;
+
+        // Draws
+        for x in 0..game_grid.cols {
+            for y in 0..game_grid.rows {
+                if game_grid.grid[x][y] {
+                    stdout
+                        .queue(cursor::MoveTo(x as u16, y as u16))?
+                        .queue(style::Print("█"))?;
+                }
+            }
+        }
+        stdout.flush()?;
+        thread::sleep(time::Duration::from_millis(250));
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,13 @@
-use crossterm::{cursor, style, terminal, ExecutableCommand, QueueableCommand};
-use std::io::{self, Write};
+use crossterm::terminal;
+use std::process;
 
-use rand::Rng;
-
-struct GameOfLife {
-    rows: usize,
-    cols: usize,
-    grid: Vec<Vec<bool>>,
-}
-
-impl GameOfLife {
-    fn new(rows: usize, cols: usize) -> Self {
-        GameOfLife {
-            rows,
-            cols,
-            grid: vec![vec![false; rows]; cols],
-        }
-    }
-
-    fn step(&mut self) {
-        todo!("simulation stepping");
-    }
-
-    fn is_extinct(&self) -> bool {
-        for x in 0..self.cols {
-            for y in 0..self.rows {
-                if self.grid[x][y] {
-                    return false;
-                }
-            }
-        }
-        true
-    }
-}
+use rustoflife::GameOfLife;
 
 fn main() {
-    // terminal::enable_raw_mode().unwrap();
     let (cols, rows) = terminal::size().unwrap();
 
-    let mut stdout = io::stdout();
-
-    let mut game_grid = GameOfLife::new(rows as usize, cols as usize);
-
-    for x in 0..game_grid.cols {
-        for y in 0..game_grid.rows {
-            game_grid.grid[x][y] = rand::thread_rng().gen_bool(0.5);
-        }
+    if let Err(e) = rustoflife::run(GameOfLife::new(rows as usize, cols as usize)) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
     }
-
-    stdout
-        .execute(terminal::Clear(terminal::ClearType::All))
-        .unwrap();
-
-    // Run until the last cell dies
-    while !game_grid.is_extinct() {
-        game_grid.step();
-
-        // Draws
-        for x in 0..game_grid.cols {
-            for y in 0..game_grid.rows {
-                if !game_grid.grid[x][y] {
-                    stdout
-                        .queue(cursor::MoveTo(x as u16, y as u16))
-                        .unwrap()
-                        .queue(style::Print("#"))
-                        .unwrap();
-                }
-            }
-        }
-        stdout.flush().unwrap();
-    }
-
-    // terminal::disable_raw_mode().unwrap();
 }
